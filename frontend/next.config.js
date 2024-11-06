ff963e2647a8bdb877d31d8bc08e44b4fefe3885
Elijah Edington <cyberpsilosis@protonmail.ch>
2024-11-05 16:20:41 -0800

    New

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname, 'src'),
-    }
-    return config
-  },
+  swcMinify: true,
 }
 
 module.exports = nextConfig 
