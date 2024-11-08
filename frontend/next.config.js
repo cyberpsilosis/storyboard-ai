839a83912f820f93705f9fd8a3bb05405c9ce05d
Elijah Edington <cyberpsilosis@protonmail.ch>
2024-11-08 01:55:42 -0800

    new

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,16 +8,6 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'react': require.resolve('react'),
-      'react-dom': require.resolve('react-dom'),
-      'react/jsx-runtime': require.resolve('react/jsx-runtime'),
-      'react/jsx-dev-runtime': require.resolve('react/jsx-dev-runtime')
-    }
-    return config
   }
 }
 
